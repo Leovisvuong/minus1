1ab2985f5bc5258f87fef815a2994378d892004c
EFTC-NTMK <vbtst001@gmail.com>
2025-11-18 17:44:00 +0700

    to first

--- main.cpp
+++ main.cpp
@@ -6,15 +6,10 @@
 #define pii pair<int,int>
 using namespace std;
 int n;
-
-void Input(){
-    cin>>n;
-}
-
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    Input();
+    // Input();
     //Output();
     return 0;
 }
